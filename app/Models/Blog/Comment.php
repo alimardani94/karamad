a5393f1e4b5d89f6c3d7b2d24e9d8ac59bfc3a63
mohammad <alimardani94@gmail.com>
2020-05-07 11:18:47 +0430

    add comment model

---
+++ app/Models/Blog/Comment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models\Blog;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Blog\Comment
+ *
+ * @property int $id
+ * @property int|null $user_id
+ * @property string|null $name
+ * @property string|null $email
+ * @property string|null $ip
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Blog\Comment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Blog\Comment newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Blog\Comment query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Blog\Comment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Blog\Comment whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Blog\Comment whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Blog\Comment whereIp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Blog\Comment whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Blog\Comment whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Blog\Comment whereUserId($value)
+ * @mixin \Eloquent
+ */
+class Comment extends Model
+{
+    //
+}
