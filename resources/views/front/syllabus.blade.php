0db3823a553dd4bc614ed89eb93722186a4de08a
alimardani94 <alimardani94@gmail.com>
2020-06-19 01:20:44 +0430

    show exam syllabus type in syllabus.blade.php

--- resources/views/front/syllabus.blade.php
+++ resources/views/front/syllabus.blade.php
@@ -14,6 +14,26 @@
         #syllabus_text img, #syllabus_text p {
             max-width: 100%;
         }
+
+        .question_card {
+            display: none;
+        }
+
+        .question_card:first-of-type {
+            display: block;
+        }
+
+        .fa-times.question_validation_icon {
+            position: absolute;
+            right: -13px;
+            font-size: 1.1rem;
+        }
+
+        .fa-check.question_validation_icon {
+            position: absolute;
+            right: -17px;
+            font-size: 1.1rem;
+        }
     </style>
 @endsection
 
@@ -35,8 +55,7 @@
                                             <h4 class="card-title">
                                                 <strong>{{$syllabus->title}}</strong>
                                             </h4>
-                                            <hr>
-                                            <div class="view">
+                                            <div class="">
                                                 @if($syllabus->type == \App\Enums\Syllabus\SyllabusType::Video)
                                                     <video controls class="img-fluid" width="100%">
                                                         <source src="{{$syllabus->video}}"
@@ -51,14 +70,78 @@
                                                             Your browser does not support the audio element.
                                                         </audio>
                                                     </div>
-                                                @else
+                                                @elseif($syllabus->type == \App\Enums\Syllabus\SyllabusType::Text)
                                                     <div id="syllabus_text">
-                                                        {!! $syllabus->text !!}}
+                                                        {!! $syllabus->text !!}
                                                     </div>
+                                                @else
+                                                    گزینه صحیح را انتخاب کنید
+                                                    <hr>
+                                                    @foreach($syllabus->questions as $index=>$question)
+                                                        <div class="card question_card" id="question_card_{{ $index }}">
+                                                            <div class="card-body">
+                                                                <h4 class="card-title">{!! $question->title !!}</h4>
+                                                                <input type="hidden" id="answer_{{$question->id}}"
+                                                                       value="{{$question->answer}}">
+
+                                                                <div class="card-text">
+                                                                    <div class="form-check mr-4">
+                                                                        <input type="radio" class="form-check-input"
+                                                                               id="{{$question->id}}_a" value="a"
+                                                                               name="question_{{$question->id}}">
+                                                                        <label class="form-check-label"
+                                                                               for="{{$question->id}}_a">{!! $question->a !!}</label>
+                                                                    </div>
+
+                                                                    <div class="form-check mr-4">
+                                                                        <input type="radio" class="form-check-input"
+                                                                               id="{{$question->id}}_b" value="b"
+                                                                               name="question_{{$question->id}}">
+                                                                        <label class="form-check-label"
+                                                                               for="{{$question->id}}_b">{!! $question->b !!}</label>
+                                                                    </div>
+                                                                    <div class="form-check mr-4">
+                                                                        <input type="radio" class="form-check-input"
+                                                                               id="{{$question->id}}_c" value="c"
+                                                                               name="question_{{$question->id}}">
+                                                                        <label class="form-check-label"
+                                                                               for="{{$question->id}}_c">{!! $question->c !!}</label>
+                                                                    </div>
+                                                                    <div class="form-check mr-4">
+                                                                        <input type="radio" class="form-check-input"
+                                                                               id="{{$question->id}}_d" value="d"
+                                                                               name="question_{{$question->id}}">
+                                                                        <label class="form-check-label"
+                                                                               for="{{$question->id}}_d">{!! $question->d !!}</label>
+                                                                    </div>
+                                                                </div>
+
+                                                                <br>
+                                                                <button class="btn btn-primary btn-sm submitBtn"
+                                                                        onclick="answerQuestion({{$question->id}})">
+                                                                    ارسال
+                                                                </button>
+                                                                @if ($loop->last)
+                                                                    <button class="btn btn-primary btn-sm getScoreBtn"
+                                                                            style="display: none"
+                                                                            onclick="getScore()">
+                                                                        مشاهده نتیجه
+                                                                    </button>
+                                                                @else
+                                                                    <button class="btn btn-primary btn-sm goToNextBtn"
+                                                                            style="display: none"
+                                                                            onclick="goToNext({{$index + 1}})">
+                                                                        بعدی
+                                                                    </button>
+                                                                @endif
+                                                            </div>
+
+                                                        </div>
+                                                    @endforeach
                                                 @endif
                                             </div>
 
-                                            @if($syllabus->attachments)
+                                            @if(count($syllabus->attachments()))
                                                 <hr>
                                                 <h4>پیوست ها</h4>
                                                 <div class="row wow fadeIn" data-wow-delay="0.4s"
@@ -138,3 +221,44 @@
     </section>
 
 @endsection
+
+@section('js')
+    <script>
+        let score = 0;
+        let questionsCount = {{ $syllabus->questions->count() }};
+
+        function answerQuestion(id) {
+            let value = $('input[name=question_' + id + ']:checked').val();
+            let answer = $('#answer_' + id).val();
+
+            $('#' + id + '_' + answer).parents('.form-check')
+                .prepend('<i class="far fa-check green-text question_validation_icon"></i>');
+
+            if (value !== answer) {
+                $('#' + id + '_' + value).parents('.form-check')
+                    .prepend('<i class="far fa-times red-text question_validation_icon"></i>');
+            } else {
+                score++
+            }
+
+            $('.submitBtn').hide();
+            $('.goToNextBtn').show();
+            $('.getScoreBtn').show();
+        }
+
+        function goToNext(id) {
+            $('.question_card').slideUp();
+            $('#question_card_' + id).slideDown();
+            $('.submitBtn').show();
+            $('.goToNextBtn').hide();
+            $('.getScoreBtn').hide();
+        }
+
+        function getScore() {
+            let finalScore = score * 100 / questionsCount;
+            let html = "شما به " + finalScore +  " درصد سوالات پاسخ صحیح دادید"
+            $('.question_card').html(html);
+        }
+    </script>
+@endsection
+
