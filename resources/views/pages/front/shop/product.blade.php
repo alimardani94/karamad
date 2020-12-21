c1063cede3a3e71d01581081e6ba9dd39ca4624c
alimardani94 <alimardani94@gmail.com>
2020-12-21 23:19:09 +0330

    add tab to product page

--- resources/views/pages/front/shop/product.blade.php
+++ resources/views/pages/front/shop/product.blade.php
@@ -53,62 +53,63 @@
 @endsection
 
 @section('content')
-    <div class="container mt-2 pt-2">
+    <div class="container mt-1">
         <section id="productDetails" class="pb-5">
-
             <div class="card mt-5 hoverable">
-                <div class="row mt-5">
-                    <div class="col-lg-6">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="slick-container ltr m-3">
-                                    <div id="mdb-lightbox-ui"></div>
-                                    <div class="slider slider-for mdb-lightbox no-margin">
-                                        @foreach($product->images() as $image)
-                                            <figure class="text-center">
-                                                <a href="{{ $image }}" data-size="1600x1067">
-                                                    <img alt="{{  $product->name }}" src="{{ $image }}"
-                                                         class="img-fluid mx-auto">
-                                                </a>
-                                            </figure>
-                                        @endforeach
+                <div class="row">
+                    <div class="col-lg-5">
+                        <div class="slick-container ltr m-3">
+                            <div id="mdb-lightbox-ui"></div>
+                            <div class="slider slider-for mdb-lightbox no-margin">
+                                @foreach($product->images() as $image)
+                                    <figure class="text-center">
+                                        <a href="{{ $image }}" data-size="1600x1067">
+                                            <img alt="{{  $product->name }}" src="{{ $image }}"
+                                                 class="img-fluid mx-auto">
+                                        </a>
+                                    </figure>
+                                @endforeach
+                            </div>
+                            <div class="slider slider-nav my-2">
+                                @foreach($product->images() as $image)
+                                    <div class="view">
+                                        <img alt="{{  $product->name }}" src="{{ $image }}"
+                                             class="img-fluid mx-auto">
+                                        <a>
+                                            <div class="mask rgba-white-slight"></div>
+                                        </a>
                                     </div>
-                                    <div class="slider slider-nav my-2">
-                                        @foreach($product->images() as $image)
-                                            <div class="view">
-                                                <img alt="{{  $product->name }}" src="{{ $image }}"
-                                                     class="img-fluid mx-auto">
-                                                <a>
-                                                    <div class="mask rgba-white-slight"></div>
-                                                </a>
-                                            </div>
-                                        @endforeach
-                                    </div>
-                                </div>
+                                @endforeach
                             </div>
                         </div>
                     </div>
+                    <div class="col-lg-7">
+                        <div class="m-3 mt-5">
+                            <h2 class="h2-responsive text-center text-md-right product-name font-weight-bold dark-grey-text mb-1 ml-xl-0 ml-4">
+                                <strong> {{ $product->name }} </strong>
+                            </h2>
 
-                    <div class="col-lg-5 mr-3 text-center text-md-right">
-                        <h2 class="h2-responsive text-center text-md-right product-name font-weight-bold dark-grey-text mb-1 ml-xl-0 ml-4">
-                            <strong> {{ $product->name }} </strong>
-                        </h2>
+                            <div class="my-2">
+                                @foreach($product->tags as $tag)
+                                    <span class="badge badge-success product m-2">{{ $tag->name}}</span>
+                                @endforeach
+                            </div>
 
-                        @foreach($product->tags as $tag)
-                            <span class="badge badge-success product mb-4 ml-2">{{ $tag->name}}</span>
-                        @endforeach
-                        <h3 class="h3-responsive text-center text-md-right mb-5 ml-xl-0 ml-4">
-                            <span
-                                class="red-text font-weight-bold"><strong>{{ number_format($product->price) }}</strong></span>
-                            <span class="grey-text"><small><s> {{ number_format($product->price) }}</s></small></span>
-                        </h3>
+                            <h3 class="h3-responsive text-center text-md-right my-4">
+                                <span class="red-text font-weight-bold">
+                                    <strong>{{ number_format($product->price) }}</strong>
+                                </span>
+                                <span class="grey-text">
+                                    <small><s> {{ number_format($product->price) }}</s></small>
+                                </span>
+                                تومان
+                            </h3>
 
-                        <p class="ml-xl-0 ml-4">
-                            {!! $product->description !!}
-                        </p>
+                            <p class="mt-0">
+                                {!! $product->description !!}
+                            </p>
 
-                        <div class="row mt-3 mb-4">
-                            <div class="col-md-12 text-center text-md-right text-md-right">
+                            <div class="d-flex justify-content-end mt-3 mb-4 mx-4">
                                 <a class="btn btn-primary btn-rounded"
                                    href="{{ route('shop.cart.add', ['product' => $product->id, 'count'=> '1']) }}">
                                     <i class="far fa-cart-plus ml-2" aria-hidden="true"></i>افزودن به سبد خر ید
@@ -126,48 +127,94 @@
             </div>
         </section>
 
-        <section>
-            @if($product->comments_count)
+        <div class="classic-tabs">
+            <div class="tabs-wrapper">
+                <ul class="nav tabs-blue" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link waves-light waves-effect waves-light active" data-toggle="tab"
+                           href="#detail" role="tab" aria-selected="true">
+                            جزئیات
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-light waves-effect waves-light" data-toggle="tab"
+                           href="#attributes" role="tab" aria-selected="false">
+                            ویژگی‌ها
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-light waves-effect waves-light" data-toggle="tab"
+                           href="#comments" role="tab" aria-selected="false">
+                            دیدگاه‌ها
+                        </a>
+                    </li>
 
-                <h4 class="h4-responsive dark-grey-text font-weight-bold my-5 text-center">
-                    <strong>دیدگاه ها</strong>
-                </h4>
+                </ul>
+            </div>
 
-                <hr class="mb-5">
-                <div class="comments-list text-center text-md-left">
-                    <div class="text-center my-5">
-                        <h3 class="font-weight-bold">دیدگاه
-                            <span class="badge indigo">{{ $product->comments_count }}</span>
-                        </h3>
+            <div class="tab-content card p-5">
+                <div class="tab-pane fade in active show" id="detail" role="tabpanel">
+                    <div class="w-100">
+                        {!! $product->description !!}
                     </div>
-
-                    @foreach ($product->comments as $comment)
-                        <div class="row mb-5">
-                            <div class="col-sm-2 col-12 mb-3">
-                                <img src="{{ $comment->image }}"
-                                     class="avatar rounded-circle z-depth-1-half"
-                                     alt="comment image">
-                            </div>
-                            <div class="col-sm-10 col-12">
-                                <a>
-                                    <h5 class="user-name font-weight-bold">{{ $comment->name }}</h5>
-                                </a>
-                                <div class="card-data">
-                                    <ul class="list-unstyled">
-                                        <li class="comment-date font-small">
-                                            <i class="far fa-clock-o"></i> {{ jDate($comment->created_at, 'dd MMMM yyyy') }}
-                                        </li>
-                                    </ul>
-                                </div>
-                                <p class="dark-grey-text article">
-                                    {{ $comment->body }}
-                                </p>
-                            </div>
-                        </div>
-                    @endforeach
                 </div>
-            @endif
-        </section>
+
+                <div class="tab-pane fade" id="attributes" role="tabpanel">
+                    <ul>
+                        @foreach($product->features() as $feature)
+                            <li>{{ $feature }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+
+                <div class="tab-pane fade" id="comments" role="tabpanel">
+                    @if($product->comments_count)
+                        <h4 class="h4-responsive dark-grey-text font-weight-bold my-5 text-center">
+                            <strong>دیدگاه‌ها</strong>
+                        </h4>
+
+                        <hr class="mb-5">
+                        <div class="comments-list text-center text-md-left">
+                            <div class="text-center my-5">
+                                <h3 class="font-weight-bold">دیدگاه
+                                    <span class="badge indigo">{{ $product->comments_count }}</span>
+                                </h3>
+                            </div>
+
+                            @foreach ($product->comments as $comment)
+                                <div class="row mb-5">
+                                    <div class="col-sm-2 col-12 mb-3">
+                                        <img src="{{ $comment->image }}"
+                                             class="avatar rounded-circle z-depth-1-half"
+                                             alt="comment image">
+                                    </div>
+                                    <div class="col-sm-10 col-12">
+                                        <a>
+                                            <h5 class="user-name font-weight-bold">{{ $comment->name }}</h5>
+                                        </a>
+                                        <div class="card-data">
+                                            <ul class="list-unstyled">
+                                                <li class="comment-date font-small">
+                                                    <i class="far fa-clock-o"></i> {{ jDate($comment->created_at, 'dd MMMM yyyy') }}
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <p class="dark-grey-text article">
+                                            {{ $comment->body }}
+                                        </p>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    @else
+                        <p class="dark-grey mb-4">
+                            هنوز هیچ دیدگاهی ثبت نشده است.
+                        </p>
+                    @endif
+                </div>
+
+            </div>
+        </div>
 
         @if($relatedProducts->count())
             <section id="products" class="pb-5 mt-4">
